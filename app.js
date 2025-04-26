eafc73beddec90b7ac2a7d7bd4235dfec44839eb
Ali Masoud <alimasoud827@gmail.com>
2025-04-26 22:03:19 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");    
+});
